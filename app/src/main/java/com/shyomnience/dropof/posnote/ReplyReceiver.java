acbb11f3056a4df40139eb69f412ab6b54fc0d04
Carl <roccscarlellis@gmail.com>
2017-09-05 17:01:54 +0200

    Initial commit

---
+++ app/src/main/java/com/shyomnience/dropof/posnote/ReplyReceiver.java
@@ -0,0 +1,33 @@
+package com.shyomnience.dropof.posnote;
+
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.RemoteInput;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
+import static com.shyomnience.dropof.posnote.MainActivity.NOTIFICATION_ID;
+
+public class ReplyReceiver extends BroadcastReceiver {
+    public ReplyReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle remoteInput = RemoteInput.getResultsFromIntent(intent);
+        if (remoteInput != null) {
+
+            CharSequence id = remoteInput.getCharSequence(MainActivity.KEY_NOTIFICATION_REPLY);
+
+            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
+                    .setSmallIcon(android.R.drawable.ic_dialog_info)
+                    .setContentTitle("Request received for ID: " + id);
+
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+        }
+    }
+}
